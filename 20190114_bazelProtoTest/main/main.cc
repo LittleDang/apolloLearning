6244d7c82fbb56f4163a00de41cbf2184d044a03
littleDang <857551494@qq.com>
2019-01-17 21:19:27 +0800

    20190117

--- 20190114_bazelProtoTest/main/main.cc
+++ 20190114_bazelProtoTest/main/main.cc
@@ -1,12 +1,12 @@
-#include<iostream>
-#include"main/test.pb.h"
+#include "main/test.pb.h"
+#include <iostream>
 using namespace std;
 int main()
 {
-apollo::control::PadMessage * test=new apollo::control::PadMessage();
-cout<<(test->has_header()?"Y":"N")<<endl;
-test->set_header(35);
-cout<<"hello world"<<(test->header()<<test->has_header()?"Y":"N")<<endl;
-delete test;
-return 0;
+    apollo::control::PadMessage* test = new apollo::control::PadMessage();
+    cout << (test->has_header() ? "Y" : "N") << endl;
+    test->set_header(35);
+    cout << "hello world" << (test->header() << test->has_header() ? "Y" : "N") << endl;
+    delete test;
+    return 0;
 }
